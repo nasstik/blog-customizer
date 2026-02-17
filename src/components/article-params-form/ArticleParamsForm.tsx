f66cf5af3aa731769e1fae2841ec0e79b817ba43
Anastasia Tishina <anastasiatisina798@yandex.ru>
2026-02-17 12:43:48 +0300

    первое ревью

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,18 +3,117 @@ import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import { Select } from 'src/ui/select/Select';
+import { RadioGroup } from 'src/ui/radio-group/RadioGroup';
+import { useState } from 'react';
+import type { OptionType } from 'src/constants/articleProps';
+import clsx from 'clsx';
+
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
+
+type ArticleParamsFormProps = {
+	initialState: ArticleStateType;
+	onApply: (settings: ArticleStateType) => void;
+	onReset: () => void;
+};
+
+export const ArticleParamsForm = ({
+	initialState,
+	onApply,
+	onReset,
+}: ArticleParamsFormProps) => {
+	const [formState, setFormState] = useState<ArticleStateType>(initialState);
+	const [isPanelOpen, setIsPanelOpen] = useState(false);
+
+	const handleFontChange = (option: OptionType) =>
+		setFormState({ ...formState, fontFamilyOption: option });
+
+	const handleFontSizeChange = (option: OptionType) =>
+		setFormState({ ...formState, fontSizeOption: option });
+
+	const handleWidthChange = (option: OptionType) =>
+		setFormState({ ...formState, contentWidth: option });
+
+	const handleBgColorChange = (option: OptionType) =>
+		setFormState({ ...formState, backgroundColor: option });
+
+	const handleFontColorChange = (option: OptionType) =>
+		setFormState({ ...formState, fontColor: option });
+
+	const handleApply = () => {
+		console.log('Применены настройки:', formState);
+		onApply(formState);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onReset();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
+			<ArrowButton
+				isOpen={isPanelOpen}
+				onClick={() => setIsPanelOpen(!isPanelOpen)}
+			/>
+			{isPanelOpen && (
+				<aside
+					className={clsx(styles.container, {
+						[styles.container_open]: isPanelOpen,
+					})}>
+					<form
+						className={styles.form}
+						onSubmit={(e) => {
+							e.preventDefault();
+							handleApply();
+						}}>
+						<Select
+							title='Шрифт'
+							options={fontFamilyOptions}
+							selected={formState.fontFamilyOption}
+							onChange={handleFontChange}
+							placeholder='Выберите шрифт'
+						/>
+						<RadioGroup
+							name='fontSize'
+							title='Размер шрифта'
+							options={fontSizeOptions}
+							selected={formState.fontSizeOption}
+							onChange={handleFontSizeChange}
+						/>
+						<Select
+							title='Цвет шрифта'
+							options={fontColors}
+							selected={formState.fontColor}
+							onChange={handleFontColorChange}
+						/>
+						<Select
+							title='Цвет фона'
+							options={backgroundColors}
+							selected={formState.backgroundColor}
+							onChange={handleBgColorChange}
+						/>
+						<Select
+							title='Ширина контента'
+							options={contentWidthArr}
+							selected={formState.contentWidth}
+							onChange={handleWidthChange}
+						/>
+						<div className={styles.bottomContainer}>
+							<Button title='Сбросить' type='clear' onClick={handleReset} />
+							<Button title='Применить' type='apply' onClick={handleApply} />
+						</div>
+					</form>
+				</aside>
+			)}
 		</>
 	);
 };
